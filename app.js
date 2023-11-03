0c96f5d681475cfacada79c7b036b70b5d8939ce
csm <csm009177@gmail.com>
2023-11-03 11:22:04 +0900

    make rout

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 // require http
-const http = require('http');
-const contenType = require('./contenType');
-let nomalCont =  contenType('text/html', 'utf8')
-//port 
+const http = require("http");
+const contenType = require("./mod/contenType");
+let nomalCont = contenType("text/html", "utf8");
+//port
 let port = 3217;
 
-const serv = http.createServer((req,res)=>{
-  // rout
-  if(req.method==='GET'&& req.url === '/'){
+// rout
+const serv = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/") {
     res.writeHead(200, nomalCont);
-    res.end('hello');
-    
-  }  else if(req.method==='POST'&& req.url === '/'){
-    res.writeHead(200, nomalCont)
-  } 
-})
+    res.end("hello");
+  } else if (req.method === "POST" && req.url === "/") {
+    res.writeHead(200, nomalCont);
+  }
+});
 
 serv.listen(port, () => {
-console.log(`
+  console.log(`
 http://localhost:${port}
-`)
-})
+`);
+});
---
+++ mod/contenType.js
@@ -0,0 +1,7 @@
+function contenType(type, encode){ 
+  const contenType = {
+  'Content-Type' : `${type}`,
+  'charset' : `${encode}`, }
+}
+
+module.exports = contenType;
