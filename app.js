89d00f0a5454f6abf9484656d3f33a99e73e82a3
csm <csm009177@gmail.com>
2023-11-03 10:37:26 +0900

    basic serv

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('hello')
+// require http
+const http = require('http');
+
+let port = 3217;
+
+const serv = http.createServer((req,res)=>{
+  res.end('hello')
+})
+
+serv.listen(port, () => {
+console.log(`
+http://localhost:${port}
+`)
+})
