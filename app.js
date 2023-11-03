ad22adee46e9a41c4522a6e138829f6c3bbd314e
csm <csm009177@gmail.com>
2023-11-03 11:11:07 +0900

    make rout

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 // require http
 const http = require('http');
 const contenType = require('./contenType');
-
+let nomalCont =  contenType('text/html', 'utf8')
+//port 
 let port = 3217;
 
 const serv = http.createServer((req,res)=>{
+  // rout
   if(req.method==='GET'&& req.url === '/'){
-    res.writeHead(200, contenType);
+    res.writeHead(200, nomalCont);
     res.end('hello');
-  }
+    
+  }  else if(req.method==='POST'&& req.url === '/'){
+    res.writeHead(200, nomalCont)
+  } 
 })
 
 serv.listen(port, () => {
