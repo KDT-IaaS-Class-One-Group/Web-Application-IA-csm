b35916ce61672e22ec3dfb5c7b2105f6bc8c2ad2
csm <csm009177@gmail.com>
2023-11-03 10:55:47 +0900

    serv test good

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 // require http
 const http = require('http');
+const contenType = require('./contenType');
 
 let port = 3217;
 
 const serv = http.createServer((req,res)=>{
-  res.end('hello')
+  if(req.method==='GET'&& req.url === '/'){
+    res.writeHead(200, contenType);
+    res.end('hello');
+  }
 })
 
 serv.listen(port, () => {
---
+++ rout.js
@@ -0,0 +1,3 @@
+export function routMaker(reqMethod, ){
+  if(req.)
+}
