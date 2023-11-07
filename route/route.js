c0e26dacf296112a3c280052051ef0fc8474fac5
csm <csm009177@gmail.com>
2023-11-07 17:09:48 +0900

    want save json data to save.json

--- route/route.js
+++ route/route.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const contenType = require('../mod/contenType');
 
-function rootRoute(req, res, contenType){
+function rootRoute(req, res, contenType) {
   if (req.method === "GET") {
     if (req.url === "/") {
       fs.readFile("./doc/index.html", (err, data) => {
@@ -32,6 +32,33 @@ function rootRoute(req, res, contenType){
         }
       });
     }
+  } else if (req.method === "POST" && req.url === "/json") {
+    let requestBody = '';
+    req.on('data', (chunk) => {
+      // 요청 본문 데이터 수집
+      requestBody += chunk;
+    });
+
+    req.on('end', () => {
+      try {
+        const userData = JSON.parse(requestBody);
+
+        // userData를 JSON 파일로 저장
+        fs.writeFile("./doc/save.json", JSON.stringify(userData, null, 2), (err) => {
+          if (err) {
+            console.log("JSON Error");
+          } else {
+            console.log("Data saved as userdata.json");
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ message: "Data saved successfully" }));
+          }
+        });
+      } catch (error) {
+        console.log("JSON Parsing Error");
+        res.writeHead(400, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: "Invalid JSON data" }));
+      }
+    });
   }
 }
 
