d0222a68645a777fb18892d00d86739bfe3f6357
csm <csm009177@gmail.com>
2023-11-07 15:08:02 +0900

    make entryRoute for entry by exports

---
+++ route/route.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const contenType = require("./mod/contenType");
+
+function entryRoute(req, res){
+  fs.readFile("./doc/index.html", (err, data) => {
+    if (err) {
+      console.log("index Error");
+    } else {
+      res.writeHead(200, contenType);
+      res.end(data);
+    }
+  });
+}
