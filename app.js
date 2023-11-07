8e0e7ef852d7cf49f8b8418eef09b27bda93dd8b
csm <csm009177@gmail.com>
2023-11-07 16:14:54 +0900

    want restore route

--- app.js
+++ app.js
@@ -1,26 +1,19 @@
 // require http
 const http = require("http");
-const contenType = require("./mod/contenType");
-const entryRoute = require("./route/route");
-const routMaker = require("./route/routMaker");
-
-// port
+const route = require("./route/route"); // route.js 파일을 가져옴
 let port = 3217;
 
-// route
 const serv = http.createServer((req, res) => {
   if (req.method === "GET") {
     if (req.url === "/") {
-      // entryRoute()
-      routMaker("GET", "/", readUrlFile("./doc/index.html", contenType))
-      
+      route.handleRoot(req, res);
     } else if (req.url === "./doc/index.css") {
       route.handleStyles(req, res);
     } else if (req.url === "./doc/index.js") {
       route.handleScript(req, res);
     }
   }
-})
+});
 
 serv.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
--- route/route.js
+++ route/route.js
@@ -1,15 +1,40 @@
-const fs = require('fs');
-const contenType = require("./mod/contenType");
+const fs = require("fs");
+// const contenType = require("./mod/contenType");
 
-function entryRoute(req, res){
+// 루트 경로 ("/")에 대한 처리
+function handleRoot(req, res) {
   fs.readFile("./doc/index.html", (err, data) => {
     if (err) {
-      console.log("index Error");
+      console.log("Error");
     } else {
-      res.writeHead(200, contenType);
+      res.writeHead(200, { "Content-Type": "text/html" });
       res.end(data);
     }
   });
 }
 
-module.exports = entryRoute;
+// "/styles.css"에 대한 처리
+function handleStyles(req, res) {
+  fs.readFile("./doc/index.css", (err, data) => {
+    if (err) {
+      console.log("Error");
+    } else {
+      res.writeHead(200, { "Content-Type": "text/css" });
+      res.end(data);
+    }
+  });
+}
+
+// "/script.js"에 대한 처리
+function handleScript(req, res) {
+  fs.readFile("./doc/index.js", (err, data) => {
+    if (err) {
+      console.log("Error");
+    } else {
+      res.writeHead(200, { "Content-Type": "application/javascript" });
+      res.end(data);
+    }
+  });
+}
+
+module.exports = { handleRoot, handleStyles, handleScript };
